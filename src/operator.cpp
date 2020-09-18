5afe5436709735608f0d65b30428fa1911480f0c
99002540 <71496134+99002540@users.noreply.github.com>
2020-09-18 18:52:56 +0530

    Update operator.cpp

--- src/operator.cpp
+++ src/operator.cpp
@@ -1,9 +1,7 @@
 
-   A(){}
-    A(int a,int b):x{a},y{b}{}
-    A(int a):x{a}{}
+  
  //Addition operator
-    A operator+(A& z){
+  A::  A operator+(A& z){
     A a4;
     a4.x=x+z.x;
     a4.y=y+z.y;
@@ -11,7 +9,7 @@
     }
 //subtractor operator
 
-    A operator-(A& z){
+    A::A operator-(A& z){
     A a5;
     a5.x=x-z.x;
     a5.y=y-z.y;
@@ -19,14 +17,14 @@
     }
 //Multiplication operator
 
-    A operator*(A& z){
+ A::   A operator*(A& z){
     A a8;
     a8.x=x*z.x;
     a8.y=y*z.y;
     return a8;
     }
 //Division operator
- A operator/(A& z){
+A:: A operator/(A& z){
     A a10;
     a10.x=x/z.x;
     a10.y=y/z.y;
@@ -34,7 +32,7 @@
     }
 // Modulation
 //Division operator
- A operator%(A& z){
+ A::A operator%(A& z){
     A a11;
     a11.x=x%z.x;
     a11.y=y%z.y;
@@ -42,46 +40,46 @@
     }
 
  //printing
-    void print(){
+ void A::   void print(){
     cout<<x<<" "<<y<<endl;
     }
  //equality
-    bool operator==(A &z){
+bool A::operator==(A &z){
     return (x==z.x&&y==z.y);
     }
 //unequal
-    bool operator!=(A &z){
+    bool A::operator!=(A &z){
     return (x!=z.x||y!=z.y);
     }
 //pre increment
-      void operator++(){
+      void A:: operator++(){
        x++;
        }
        //post inc
-       void operator++(int ){
+       void A::operator++(int ){
        x++;
        }
        //pre dec
-       void operator--( ){
+       void A::operator--( ){
        x--;
        }
        //post dec
-       void operator--(int ){
+       void A::operator--(int ){
        x--;
        }
-       A operator-(){
+       A::A operator-(){
        return (-x);
        }
        //display
-       void display(){
+       void A::display(){
        cout<<x<<endl;
        }
        //greater
-       bool operator>(A &z){
+       bool A:: operator>(A &z){
        return (x>z.x);
        }
        //less
-       bool operator<(A &z){
+       bool A::operator<(A &z){
        return (x<z.x);
        }
 
