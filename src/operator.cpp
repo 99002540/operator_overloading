d29c6cce9d79453f1767495b3d8e2b499a0eb32d
99002540 <71496134+99002540@users.noreply.github.com>
2020-09-18 18:22:52 +0530

    Update operator.cpp

--- src/operator.cpp
+++ src/operator.cpp
@@ -1,38 +1,92 @@
-#include<iostream>
-#include "operator.h"
-int main()
 
-{
-A a1(1,2),a2(1,3);
-A a3{a1+a2};
-A a6{a2-a1};
-A a7{a1*a2};
-//A a8=a1+3;
-a7.print();
-a3.print();
-a6.print();
-A b1(4),b2(5);
-if(a1==a2){
-    cout<<"equal"<<endl;
-}
-else  {
-    cout<<" Not equal"<<endl;
-}
-   A o1(4),o3(3);
-    ++o1;
-    o1.display();
-    o1++;
-    o1.display();
-    --o1;
-    o1.display();
-    o1--;
-    o1.display();
-    A o2=-o1;
-    o2.display();
-if(o1>o3){
-    cout<<"o1 is greater";
-}
-else{
-    cout<<"o2 is greater";
-}
-}
+
+
+public:
+   A(){}
+    A(int a,int b):x{a},y{b}{}
+    A(int a):x{a}{}
+ //Addition operator
+    A operator+(A& z){
+    A a4;
+    a4.x=x+z.x;
+    a4.y=y+z.y;
+    return a4;
+    }
+//subtractor operator
+
+    A operator-(A& z){
+    A a5;
+    a5.x=x-z.x;
+    a5.y=y-z.y;
+    return a5;
+    }
+//Multiplication operator
+
+    A operator*(A& z){
+    A a8;
+    a8.x=x*z.x;
+    a8.y=y*z.y;
+    return a8;
+    }
+//Division operator
+ A operator/(A& z){
+    A a10;
+    a10.x=x/z.x;
+    a10.y=y/z.y;
+    return a10;
+    }
+// Modulation
+//Division operator
+ A operator%(A& z){
+    A a11;
+    a11.x=x%z.x;
+    a11.y=y%z.y;
+    return a11;
+    }
+
+ //printing
+    void print(){
+    cout<<x<<" "<<y<<endl;
+    }
+ //equality
+    bool operator==(A &z){
+    return (x==z.x&&y==z.y);
+    }
+//unequal
+    bool operator!=(A &z){
+    return (x!=z.x||y!=z.y);
+    }
+//pre increment
+      void operator++(){
+       x++;
+       }
+       //post inc
+       void operator++(int ){
+       x++;
+       }
+       //pre dec
+       void operator--( ){
+       x--;
+       }
+       //post dec
+       void operator--(int ){
+       x--;
+       }
+       A operator-(){
+       return (-x);
+       }
+       //display
+       void display(){
+       cout<<x<<endl;
+       }
+       //greater
+       bool operator>(A &z){
+       return (x>z.x);
+       }
+       //less
+       bool operator<(A &z){
+       return (x<z.x);
+       }
+
+
+
