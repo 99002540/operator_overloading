44f05f85d1b813dd96966e6296d535acc25d2cc6
99002540 <71496134+99002540@users.noreply.github.com>
2020-09-18 19:59:50 +0530

    Update operator.cpp

--- src/operator.cpp
+++ src/operator.cpp
@@ -1,11 +1,11 @@
 
   
  //Addition operator
-    void operator+(A& x,A& y){
+    A operator+(A& x,A& y){
     A a4;
     a4.x=x+z.x;
     a4.y=y+z.y;
-    cout<<a4.x <<a4.y<<endl;
+    return a4;
     }
 
 
