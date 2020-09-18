cff51391c0b2124d3252e3aa21e7af202dc37597
99002540 <71496134+99002540@users.noreply.github.com>
2020-09-18 22:27:25 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,11 @@
   
 #include<iostream>
-using namespace std;
-#include "operator.h"
-int main()
 
+#include "operator.h"
+using namespace std;
+int main()
 {
 A a1(1,2),a2(1,3);
 A a3{a1+a2};
-//A a6{a2-a1};
-//A a7{a1*a2};
-//A a8=a1+3;
- return 0;
+return 0;
 }
