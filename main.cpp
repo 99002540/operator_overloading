d2422958b4bbe28aa258affd3791bf4b59a49045
99002540 <71496134+99002540@users.noreply.github.com>
2020-09-18 19:32:47 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,31 +10,4 @@ A a3{a1+a2};
 A a6{a2-a1};
 A a7{a1*a2};
 //A a8=a1+3;
-a7.print();
-a3.print();
-a6.print();
-A b1(4),b2(5);
-if(a1==a2){
-    cout<<"equal"<<endl;
-}
-else  {
-    cout<<" Not equal"<<endl;
-}
-   A o1(4),o3(3);
-    ++o1;
-    o1.display();
-    o1++;
-    o1.display();
-    --o1;
-    o1.display();
-    o1--;
-    o1.display();
-    A o2=-o1;
-    o2.display();
-if(o1>o3){
-    cout<<"o1 is greater";
-}
-else{
-    cout<<"o2 is greater";
-}
 }
