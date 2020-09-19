a0a00a32ff5fb32b0ae6191939e97ca32bd18f6a
99002540 <71496134+99002540@users.noreply.github.com>
2020-09-19 07:07:35 +0530

    Update operator.cpp

--- src/operator.cpp
+++ src/operator.cpp
@@ -6,5 +6,76 @@
     a4.y=y+z.y;
     return a4;
     }
+ A A::A operator-(A& z){
+    A a4;
+    a4.x=x-z.x;
+    a4.y=y-z.y;
+    return a4;
+    }
+     A A::A operator*(A& z){
+    A a4;
+    a4.x=x*z.x;
+    a4.y=y*z.y;
+    return a4;
+    }
+     A A::A operator/(A& z){
+    A a4;
+    a4.x=x/z.x;
+    a4.y=y/z.y;
+    return a4;
+    }
+     A A::A operator+(A& z){
+    A a4;
+    a4.x=x%z.x;
+    a4.y=y%z.y;
+    return a4;
+    }
+
+void print(){
+    cout<<x<<" "<<y<<endl;
+    }
+ //equality
+bool A::operator==(A &, A&){
+    return (x==z.x&&y==z.y);
+    }
+//unequal
+    bool A::operator!=(A &z){
+    return (x!=z.x||y!=z.y);
+    }
+//pre increment
+      void A:: operator++(A& x){
+       x++;
+       }
+       //post inc
+       void A::operator++(int A& x){
+       x++;
+       }
+       //pre dec
+       void A::operator--( A& x){
+       x--;
+       }
+       //post dec
+       void A::operator--(int A& x){
+       x--;
+       }
+       A::A operator-(A& x){
+       return (-x);
+       }
+       //display
+       void A::display(){
+       cout<<x<<endl;
+       }
+       //greater
+       bool A:: operator>(A &z){
+       return (x>z.x);
+       }
+       //less
+       bool A::operator<(A &z){
+       return (x<z.x);
+       }
+
+
+
+
 
 
